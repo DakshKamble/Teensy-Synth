7b449d0bbbccfb4b57565b96dbe7458618b99842
Daksh Kamble <63111617+DakshKamble@users.noreply.github.com>
2026-03-28 09:04:46 +0530

    Add getSineWave and getModulator function
    - modulated sine wave using another getModulator

--- Teensy-Synth-Firmware/src/main.cpp
+++ Teensy-Synth-Firmware/src/main.cpp
@@ -7,8 +7,16 @@ float phase = 0.0;
 float freq = 200.0;
 float sampleRate = 10000.0;
 
+//For modulating main sine wave
+float modMax = 400.0;
+float modMin = 0.0;
+float modPhase = 0.0;
+
+
 //Function declarations
 void writeDac(uint16_t value);
+float getSineWave(float frequency);
+float getModulator(float frequency);
 
 void setup() {
   pinMode(CS, OUTPUT);
@@ -18,14 +26,19 @@ void setup() {
 }
 
 void loop() {
-  phase += 0.05; //Controls speed
-  if (phase > 2 * PI) {
-  phase -= 2 * PI;
-  }
-  float s = sin(phase);
-  uint16_t value = (s + 1)* 2047; 
+  // 1. Get a value between -1.0 and 1.0 from the slow wave (0.5 Hz)
+  float modValue = getModulator(0.5); 
 
-  writeDac(value);
+  // 2. Map that -1 to 1 value to your 200 to 400 range
+  // Middle is 300, spread is 100
+  float targetFreq = 300.0 + (modValue * 100.0);
+
+  // 3. Feed that "Target Frequency" into your main sine wave function
+  float finalSignal = getSineWave(targetFreq);
+
+  // 4. Send to DAC
+  writeDac((finalSignal + 1.0) * 2047);
+
   delayMicroseconds(100);
 }
 
@@ -33,6 +46,23 @@ void loop() {
 
 
 
+
+
+float getSineWave(float frequency) {
+  static float phase = 0.0;
+  float phaseInc = (2.0 * PI * frequency) / 10000.0;
+  phase += phaseInc;
+  if (phase > 2.0 * PI) phase -= 2.0 * PI;
+  return sin(phase);
+}
+
+float getModulator(float frequency) {
+  static float p_mod = 0.0; // Separate position for the modulator
+  float phaseInc = (2.0 * PI * frequency) / 10000.0; 
+  p_mod += phaseInc;
+  if (p_mod > 2.0 * PI) p_mod -= 2.0 * PI;
+  return sin(p_mod); 
+}
 
 void writeDac(uint16_t value) {
   uint16_t command = 0x3000 | (value & 0x0fff); //Define the 16bit packet for sending
